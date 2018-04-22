658640f371a368be62d0d2400548fcf33676d5e8
Bill Neely <bill.neely.tx@gmail.com>
2018-04-22 14:32:10 -0500

    Zoo and Exhibit models

--- pythonzoo/zoo/admin.py
+++ pythonzoo/zoo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Zoo, Exhibit
+
 # Register your models here.
+
+
+class ZooAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+admin.site.register(Zoo, ZooAdmin)
+
+class ExhibitAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+admin.site.register(Exhibit, ExhibitAdmin)
--- pythonzoo/zoo/models.py
+++ pythonzoo/zoo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Zoo(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter Zoo Name")
+	
+	def __str__ (self):
+		return self.name
+
+class Exhibit(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter Exhbit Name")
+	zoo = models.ForeignKey('Zoo', on_delete=models.SET_NULL, null=True) 
+	
